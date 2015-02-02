883ae358916add5711c9f120b3877130c8159d7b
Harris Tzovanakis <me@drjova.com>
2015-02-02 18:13:13 +0100

    initial
    Signed-off-by: Harris Tzovanakis <me@drjova.com>

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # cds-backup
 Static backup site for CDS in case of maintenance and/or unexpected downtime.
+
+## Instalation
+```bash
+# Inside the project
+npm install;
+brew install;
+grunt build;
+```
+The files are under `dist` directory.
+
+### For development
+
+The sources are under `app` directory.
+
+```bash
+grunt serve;
+```
---
+++ app/scripts/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+  var available = [
+    {
+      "image": "73-11-155.jpg",
+      "title": "Ambulance after crash"
+    },
+    {
+      "image": "64-4-013.jpg",
+      "title": "Cleaning operation during the Synchro-Cyclotron (SC) shut-down"
+    }
+  ];
+  var random = Math.floor((Math.random() * available.length) + 1);
+  console.log(available[random-1]);
+  var selected = available[random-1];
+  $.backstretch("/images/"+selected.image);
+  $('.cds-image-caption').find('.text').first().text(selected.title);
+})
